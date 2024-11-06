aed45ccdf85943be8ac34f3a361ae1de1b5fcb6d
mustafagarko <keligov.m@mail.ru>
2024-11-06 21:37:56 +0300

    fix build for CI

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -21,13 +21,14 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
+
     ];
 
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
     }
     return plugins;
 }
