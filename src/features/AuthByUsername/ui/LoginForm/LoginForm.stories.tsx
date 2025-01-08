eaa8fa3fb25269b6ec2c59da55b67090f6483883
mustafagarko <keligov.m@mail.ru>
2025-01-08 21:40:11 +0300

    add precommit

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { LoginForm } from './LoginForm';
+
+export default {
+    title: 'feature/LoginForm',
+    component: LoginForm,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof LoginForm>;
+
+const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+
+};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+
+};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
