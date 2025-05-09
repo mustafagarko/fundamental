da5460908b4dc398ecb60e3068e70f4cbf23104a
mustafagarko <keligov.m@mail.ru>
2025-05-09 23:20:50 +0300

    husky pre-commit try

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,13 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
 import cls from './MainPage.module.scss';
 
 export default function MainPage() {
     const { t } = useTranslation('main');
     return (
         <div className={cls.MainPage}>
+            <p>
+                {t('MainTitle')}
+            </p>
             <p>
                 {t('MainTitle')}
             </p>
