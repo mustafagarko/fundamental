00aa13147e1e7fb44cd8ea0cce56831826466056
mustafagarko <keligov.m@mail.ru>
2025-06-15 19:09:16 +0300

    add tests for loginByUsername and loginSlice

---
+++ src/features/AuthByUsername/model/services/loginByUsername.ts/loginByUsername.test.ts
@@ -0,0 +1,60 @@
+import { Dispatch } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { userActions } from 'entities/User';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { loginByUsername } from './loginByUsername';
+
+jest.mock('axios');
+const mockedAxios = jest.mocked(axios, true);
+
+describe('loginByUsername.test', () => {
+    // let dispatch: Dispatch;
+    // let getState: () => StateSchema;
+
+    // beforeEach(() => {
+    //     dispatch = jest.fn();
+    //     getState = jest.fn();
+    // });
+    // test('success login', async () => {
+    //     const userValue = { username: '123', id: '1' };
+    //     mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+    //     const action = loginByUsername({ username: '123', password: '123' });
+    //     const result = await action(dispatch, getState, undefined);
+    //     console.log(result);
+    //     expect(result.meta.requestStatus).toBe('fulfilled');
+    //     expect(mockedAxios.post).toHaveBeenCalled();
+    //     expect(dispatch).toHaveBeenCalledWith(userActions.setUserData(userValue));
+    //     expect(dispatch).toHaveBeenCalledTimes(3);
+    //     expect(result.payload).toBe(userValue);
+    // });
+    // test('error login', async () => {
+    //     mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+    //     const action = loginByUsername({ username: '123', password: '123' });
+    //     const result = await action(dispatch, getState, undefined);
+    //     expect(result.meta.requestStatus).toBe('rejected');
+    //     expect(mockedAxios.post).toHaveBeenCalled();
+    //     expect(dispatch).toHaveBeenCalledTimes(2);
+    //     expect(result.payload).toBe('error');
+    // });
+    test('success login', async () => {
+        const userValue = { username: '123', id: '1' };
+        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: '123', password: '123' });
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setUserData(userValue));
+        expect(thunk.dispatch).toHaveBeenCalledTimes(3);
+        expect(result.payload).toBe(userValue);
+    });
+    test('error login', async () => {
+        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
+        const thunk = new TestAsyncThunk(loginByUsername);
+        const result = await thunk.callThunk({ username: '123', password: '123' });
+        expect(result.meta.requestStatus).toBe('rejected');
+        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+        expect(result.payload).toBe('error');
+    });
+});
---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,14 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { loginActions, loginReducer } from './loginSlice';
+import { LoginSchema } from '../types/loginSchema';
+
+describe('loginSlice.test', () => {
+    test('set username test', () => {
+        const state: DeepPartial<LoginSchema> = { username: '12' };
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('12345'))).toEqual({ username: '12345' });
+    });
+    test('set password test', () => {
+        const state: DeepPartial<LoginSchema> = { password: '12' };
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('12345'))).toEqual({ password: '12345' });
+    });
+});
